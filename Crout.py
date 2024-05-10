da991489c5f5c3d6eedad2332ac59219a364b5db
fadlishidqi <118976398+fadlishidqi@users.noreply.github.com>
2024-05-10 15:01:55 +0700

    Update Crout.py

--- Crout.py
+++ Crout.py
@@ -2,19 +2,17 @@ import numpy as np
 
 # Metode dekomposisi Crout
 def crout(A, b):
-    n = len(A) 
-    L = np.zeros((n, n)) 
-    U = np.zeros((n, n)) 
+    n = len(A)
+    L = np.zeros((n, n))
+    U = np.zeros((n, n))
 
     for i in range(n):
         L[i, i] = 1
-    for j in range(n):
-        for i in range(j, n):
-            U[i, j] = A[i, j] - sum(L[i, k] * U[k, j] for k in range(j))
-    for j in range(n):
-        for i in range(j+1, n):
-            L[i, j] = (A[i, j] - sum(L[i, k] * U[k, j] for k in range(i))) / U[j, j]
-    
+        for j in range(i, n):
+            U[i, j] = A[i, j] - sum(L[i, k] * U[k, j] for k in range(i))
+        for j in range(i+1, n):
+            L[j, i] = (A[j, i] - sum(L[j, k] * U[k, i] for k in range(i))) / U[i, i]
+
     # Solution
     y = np.linalg.solve(L, b.astype(float))
     x = np.linalg.solve(U, y)
